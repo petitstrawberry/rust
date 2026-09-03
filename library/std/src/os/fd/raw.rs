8d1e61697d42dd6c08ca40fb15eb6af20769ccb3
petitstrawberry <petitstrawberry.dev@gmail.com>
2026-09-04 00:59:58 +0900

    Fix scarlet std doc build: gate unix::io::AsFd doc import on unix

--- library/std/src/os/fd/raw.rs
+++ library/std/src/os/fd/raw.rs
@@ -16,7 +16,7 @@ use crate::io;
 use crate::os::hermit::io::OwnedFd;
 #[cfg(all(not(target_os = "hermit"), not(target_os = "motor")))]
 use crate::os::raw;
-#[cfg(all(doc, not(target_arch = "wasm32")))]
+#[cfg(all(doc, unix, not(target_arch = "wasm32")))]
 use crate::os::unix::io::AsFd;
 #[cfg(unix)]
 use crate::os::unix::io::OwnedFd;
